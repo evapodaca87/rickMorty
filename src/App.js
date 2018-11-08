3c2173423092015e95f0b47890efcb83f12386f9
evapodaca87 <evapodaca87@gmail.com>
2018-11-08 15:10:59 -0700

    finally got fecth request working PAGE WORKING

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Input } from 'semantic-ui-react';
-import CardList from './CardList';
+import Card from './Card';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: [ 1, 2, 3 ]
+            cards: []
         };
     }
-
-    async componentDidMount() {
-        const cardFetch = await fetch('https://rickandmortyapi.com/api/character');
-        const cardJson = await cardFetch.json();
-        this.setState({ cards: cardJson });
-        console.log(this.state.cards);
+    componentDidMount() {
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+            targetUrl = 'https://rickandmortyapi.com/api/character';
+        fetch(proxyUrl + targetUrl).then((res) => res.json()).then((data) => {
+            this.setState({ cards: data.results });
+        });
     }
 
     render() {
@@ -30,7 +30,7 @@ class App extends Component {
                 <Input className='search' size='large' icon='search' placeholder='Search...' />
                 <br />
                 <br />
-                <CardList cards={this.state.cards} />
+                <Card cards={this.state.cards} />
             </div>
         );
     }
--- src/Card.js
+++ src/Card.js
@@ -1,35 +1,37 @@
 import React from 'react';
 
-const Card = (props) => {
-    return (
-        <figure className='snip1579'>
-            <img
-                className='boxes'
-                src='https://rickandmortyapi.com/api/character/avatar/150.jpeg'
-                alt='profile-sample2'
-            />
-            <figcaption>
-                <h1>Name</h1>
-                <br />
-                <div className='split'>
-                    <div>
-                        Species
-                        <div>Type</div>
-                        <div>Alive?</div>
+const Card = ({ cards }) => {
+    return cards.map((card) => {
+        return (
+            <figure className='snip1579'>
+                <img
+                    className='boxes'
+                    src='https://rickandmortyapi.com/api/character/avatar/150.jpeg'
+                    alt='profile-sample2'
+                />
+                <figcaption>
+                    <h1>{card.name}</h1>
+                    <br />
+                    <div className='split'>
+                        <div>
+                            Species
+                            <div>Type</div>
+                            <div>Alive?</div>
+                        </div>
+                        <div>
+                            Planet
+                            <div>Origin</div>
+                            <div>Last Seen</div>
+                        </div>
                     </div>
-                    <div>
-                        Planet
-                        <div>Origin</div>
-                        <div>Last Seen</div>
-                    </div>
-                </div>
-                <div>Gender</div>
-                <br />
-                <div className='episode'>Episode</div>
-                <br />
-            </figcaption>
-        </figure>
-    );
+                    <div>Gender</div>
+                    <br />
+                    <div className='episode'>Episode</div>
+                    <br />
+                </figcaption>
+            </figure>
+        );
+    });
 };
 
 export default Card;
