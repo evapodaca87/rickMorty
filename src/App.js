98eac9cef932e306b65f75188cf4f3a58d59b18b
evapodaca87 <evapodaca87@gmail.com>
2018-11-08 15:29:59 -0700

    added event listener on search bar PAGE WORKING

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ class App extends Component {
             cards: []
         };
     }
+
+    search = (e) => {
+      console.log(e.target.value)
+    }
     componentDidMount() {
         var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
             targetUrl = 'https://rickandmortyapi.com/api/character';
@@ -27,7 +31,7 @@ class App extends Component {
                     src='https://mbtskoudsalg.com/images/rick-and-morty-logo-png-1.png'
                 />
                 <h1 className='head'>API Library</h1>
-                <Input className='search' size='large' icon='search' placeholder='Search...' />
+                <Input onChange={this.search} className='search' size='large' icon='search' placeholder='Search...' />
                 <br />
                 <br />
                 <Card cards={this.state.cards} />
